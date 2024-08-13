aaf61e31aba03f662e61759a52219ff8c7390b9d
jhow_maior <jhow_maior@msn.com>
2024-08-13 00:05:08 -0300

    remove headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless:false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.saucedemo.com',
 
